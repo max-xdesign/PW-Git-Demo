0ffcb58fe254ffda1d0f12d686d8407b95743613
Max Sadler <max.sadler@xdesign.com>
2024-09-10 16:46:34 +0100

    added a dropdown test

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 
-// finding text on the page
+// finding text on the home page
 test('homepage has header', async ({ page }) => {
   await page.goto('https://createfuture.com/');
 
   await expect(page.getByText('So, what are we ')).toBeVisible();
 });
 
-//click on a link
+//click on a link to arrive at contact page
 test('click link on homepage', async ({ page }) => {
   await page.goto('https://createfuture.com/');
 
@@ -17,11 +17,20 @@ test('click link on homepage', async ({ page }) => {
   await expect(page.getByText('Get in touch')).toBeVisible();
 });
 
-//find an image
+//find an image on the about page
 test('check background image', async ({ page }) => {
   await page.goto('https://createfuture.com/about/');
 
   const aboutImage = page.locator('.background-image')
 
   await expect(aboutImage).toBeVisible();
+});
+
+
+//test dropdown elements to visit Services Page
+test('Dropdown navigation test', async ({ page }) => {
+  await page.goto('https://createfuture.com/');
+  await page.getByRole('link', { name: 'Our Services'}).hover();
+  await page.getByRole('link', { name: 'Data and AI' }).click();
+  await expect(page.getByText('Data and AI')).toBeVisible;
 });
