47f22a84993e5f52f852af54e70186f42ac563f0
Max Sadler <max.sadler@xdesign.com>
2024-09-10 17:09:29 +0100

    added alt text image test on case study

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -17,7 +17,7 @@ test('click link on homepage', async ({ page }) => {
   await expect(page.getByText('Get in touch')).toBeVisible();
 });
 
-//find an image on the about page
+//find a missing image on the about page without alt text
 test('check background image', async ({ page }) => {
   await page.goto('https://createfuture.com/about/');
 
@@ -34,3 +34,12 @@ test('Dropdown navigation test', async ({ page }) => {
   await page.getByRole('link', { name: 'Data and AI' }).click();
   await expect(page.getByText('Data and AI')).toBeVisible;
 });
+
+//find an image on a case study using alt text
+test('find image by alt text', async ({ page }) => {
+  await page.goto('https://createfuture.com/');
+  await page.getByRole('link', { name: 'Our Experience'}).hover();
+  await page.getByRole('link', { name: 'adidas' }).click();
+  await expect(page.getByAltText('Adidas user testing')).toBeVisible;
+});
+
