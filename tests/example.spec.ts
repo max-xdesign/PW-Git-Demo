7aa6ee972731ff1bf42c8fc2c5d2884bb2f3cf26
Max Sadler <max.sadler@xdesign.com>
2024-09-10 14:41:33 +0100

    added new click test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,18 @@
 import { test, expect } from '@playwright/test';
 
+
+// finding text on the page
 test('homepage has header', async ({ page }) => {
   await page.goto('https://createfuture.com/');
 
   await expect(page.getByText('So, what are we ')).toBeVisible();
+});
+
+//click on a link
+test('click link on homepage', async ({ page }) => {
+  await page.goto('https://createfuture.com/');
+
+  await page.getByText("Let's talk").click();
+
+  await expect(page.getByText("Let's talk action")).toBeVisible();
 });
